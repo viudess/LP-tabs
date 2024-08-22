cac85029f83c9263b1486d82a9e27ebe57cd624c
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-22 11:08:37 -0300

    metade do projeto

---
+++ src/app/model/IFilmes.ts
@@ -0,0 +1,11 @@
+/* eslint-disable eol-last */
+export interface IFilmes {
+  nome: string;
+  lancamento: string;
+  duracao: string;
+  classificacao: number;
+  cartaz: string;
+  generos: string[];
+  pagina?: string;
+  favorito: boolean;
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
+import { IFilmes } from './../model/IFilmes';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-tab1',
@@ -8,5 +10,28 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+
+  listaFilmes: IFilmes[] = [
+    {
+      nome: 'Mortal Kombat (2021)',
+      lancamento: '15/04/2021',
+      duracao: '1h50m',
+      classificacao: 6,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/44aCR6cjH0FbzW6PMw3Ega178iW.jpg',
+      generos: ['Ação', 'Fantasia', 'Aventura'],
+      pagina: '/mortal-kombat',
+      favorito: false
+    },
+    {
+      nome: 'Vingadores: Ultimato (2019)',
+      lancamento: '25/04/2019 (BR)',
+      duracao: '3h01m',
+      classificacao: 9,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9fRX8UKlIW7Lb9GqNsJVakWWFCi.jpg',
+      generos: ['Aventura', 'Ficção científica', 'Ação'],
+      pagina: '/avengers',
+      favorito: false
+    }
+  ];
 
 }
